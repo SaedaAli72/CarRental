f1f7952a5a8b57185e6686ebf673567ad20abb8b
AmrAlaraby <amr2amr3@gmail.com>
2026-02-09 22:25:31 +0200

    Edit Dashboard adding cars

---
+++ CarRentalPL/Controllers/AmrController.cs
@@ -0,0 +1,21 @@
+﻿using CarRentalBLL.Services.Interface;
+using CarRentalBLL.ViewModels.Car;
+using Microsoft.AspNetCore.Mvc;
+
+namespace CarRentalPL.Controllers
+{
+    public class AmrController : Controller
+    {
+        private readonly ICarService _carService;
+
+        public AmrController(ICarService carService)
+        {
+            _carService = carService;
+        }
+        public IActionResult Index()
+        {
+            List<CarCardVM> allCars =_carService.GetAllCars(null).ToList();
+            return View("index" , allCars);
+        }
+    }
+}
